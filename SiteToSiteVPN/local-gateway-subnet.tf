1d5534747d542b7fa9464b28359299b36adc9a9c
Harsh Bhargava <93305037+Harsh5006@users.noreply.github.com>
2024-05-24 14:00:13 +0530

    Delete SiteToSiteVPN directory

--- SiteToSiteVPN/local-gateway-subnet.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_local_network_gateway" "onpremise" {
-    name = "onpremise"
-    location = azurerm_resource_group.myrg.location
-    resource_group_name = azurerm_resource_group.myrg.name
-    gateway_address = var.gateway_address
-    address_space = var.local_gateway_address_space
-}
--- SiteToSiteVPN/virtual-network.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-    name = var.virtual_network_name
-    address_space = var.virtual_network_address_space
-    location = var.resources_location
-    resource_group_name = azurerm_resource_group.myrg.name
-}
